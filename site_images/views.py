3ebf0814238fcf2e42ff5bd5f6b384675fdc2244
tuuganbekov <tuuganbekovb@gmail.com>
2022-04-20 18:20:33 +0600

    refactor imageApiView, get images by type

--- site_images/views.py
+++ site_images/views.py
@@ -2,10 +2,18 @@ from rest_framework.views import APIView
 from rest_framework import response, status
 
 from site_images.models import Image
+from site_images.serializers import ImageSerializer
 
 
 class ImagesApiView(APIView):
 
     def get(self, request, format=None):
-        params = request.query_params
-        return response.Response(status=status.HTTP_200_OK)
+        # query params image type id
+        image_type = request.query_params.get('type')
+        try:
+            image = Image.objects.get(image_type=image_type)
+            serializer = ImageSerializer(image)
+            return response.Response(serializer.data, status=status.HTTP_200_OK)
+        except Image.DoesNotExist:
+            return response.Response({"error_message": "Image does not exist"}, status=status.HTTP_400_BAD_REQUEST)
+        
