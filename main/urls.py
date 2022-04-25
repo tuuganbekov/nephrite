11e19ac85f28bf5dafa81aedf0335423fbd6c7aa
tuuganbekov <tuuganbekovb@gmail.com>
2022-04-25 15:07:53 +0600

    add swagger

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Nephrite')
 
 
 urlpatterns = [
@@ -26,4 +30,5 @@ urlpatterns = [
     path('api/v1/orders/', include('orders.urls')),
     path('api/v1/site-images/', include('site_images.urls')),
     path('api/v1/promotions/', include('promotions.urls')),
+    url(r'^$', schema_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
