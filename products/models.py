4b9fcff05fa52ccecac18e7a8cc378d5f239859b
tuuganbekov <tuuganbekovb@gmail.com>
2022-04-25 12:36:00 +0600

    refactor models add verbose names to all models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 from categories.models import Category
@@ -5,15 +6,19 @@ from categories.models import Category
 
 # Create your models here.
 class Product(models.Model):
-    title = models.CharField(max_length=250)
-    image = models.ImageField(upload_to='media/')
-    description = models.TextField()
-    old_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
-    new_price = models.DecimalField(max_digits=7, decimal_places=2)
-    quantity = models.IntegerField(null=True, blank=True)
-    category = models.ManyToManyField(Category, related_name='products')
-    is_active = models.BooleanField(default=True)
+    title = models.CharField(max_length=250, verbose_name='Название')
+    image = models.ImageField(upload_to='media/', verbose_name='Картинка')
+    description = models.TextField(verbose_name='Описание')
+    old_price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True, verbose_name='Старая цена')
+    new_price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Новая цена')
+    quantity = models.IntegerField(null=True, blank=True, verbose_name='Количество')
+    category = models.ManyToManyField(Category, related_name='products', verbose_name='Продукты')
+    is_active = models.BooleanField(default=True, verbose_name='Активный')
 
     def __str__(self) -> str:
         return self.title
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
         
