707ec9c1512b23cd14f0b06c21299360e1ab900c
tuuganbekov <tuuganbekovb@gmail.com>
2022-04-25 10:57:03 +0600

    add Procfile, change allowed hosts, add gunicorn, whitenoise

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nephrite.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -131,7 +131,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
